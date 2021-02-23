3a584a291765ac3f6d9324b674335648ecf9ce3e
Alex Berumen <alexandroberumen@gmail.com>
2021-02-22 18:44:40 -0800

    added image and details to about section

---
+++ src/Components/imageSection.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import about from '../images/gradPic.jpg'
+
+const ImageSection = () => {
+    return (
+        <div className='ImageSection'>
+            <div className="img">
+                <img src={about} alt="/" />
+            </div>
+            <div className="about-info">
+                <h4>I am <span> Alex Berumen.</span></h4>
+                <p>Graduated from UC Berkeley with a degree in Molecular Biology, but have always had my eye on a seperate field. As I spent more and more of my free time learning about the different aspects of software, I realized I wanted a career change.
+                    The constant shifts and everchaning landscape in software really intrigued me and once I started building, I knew this was the path I needed to go down. Through the struggles and tiny achievments along the way, I've really enjoyed my journey and look forward to the progress I will make.</p>
+                <div className="about-details">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ImageSection
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Title from '../Components/Title'
+import ImageSection from '../Components/imageSection'
 
 const About = () => {
     return (
         <div className="AboutPage">
             <Title title={'About Me'} span={'About Me'} />
+            <ImageSection />
         </div>
     )
 }
