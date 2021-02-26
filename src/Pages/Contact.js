27f5bbf963ddbb8ad9e4f1d2ea8ddd5dce8dbc3e
Alex Berumen <alexandroberumen@gmail.com>
2021-02-25 19:22:12 -0800

    finished contact page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import ContactItem from '../Components/ContactItem'
+import phone from '../images/smartphone.svg'
+import email from '../images/send.svg'
+
+
 
 const Contact = () => {
     return (
-        <div>
-            contact
+        <div className='ContactPage'>
+            <div className="contact-sect">
+                <ContactItem icon={phone} text1={'(209)568-6070'} title={'Phone'} />
+                <ContactItem icon={email} text1={'alexandroberumen@gmail.com'} title={'Email'} />
+
+
+
+
+            </div>
         </div>
     )
 }
