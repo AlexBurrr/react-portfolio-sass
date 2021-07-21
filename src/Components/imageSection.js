5cd634bf761ba7cb46aa1614d08d0bdd66fefcb9
Alex Berumen <alexandroberumen@gmail.com>
2021-07-20 20:58:51 -0700

    h

--- src/Components/imageSection.js
+++ src/Components/imageSection.js
@@ -22,9 +22,9 @@ const ImageSection = () => {
                 <h4>I am <span> Alex Berumen.</span></h4>
                 <p>I graduated from UC Berkeley with a degree in Molecular Biology, but have always had my eye on a seperate field. As I spent more and more of my free time learning about the different aspects of software, I realized I wanted a career change.
                     The constant shifts and everchaning landscape in software really intrigued me and once I started building, I knew this was the path I needed to go down. Through the struggles and tiny achievments along the way, I've really enjoyed my journey and look forward to the progress I will make.</p>
-                <form method='get' action="../">
-                    <button className='btn' onClick={ResumeDownload}>Download CV</button>
-                </form>
+
+                <button className='btn' onClick={ResumeDownload}>Download CV</button>
+
 
             </div>
         </div>
