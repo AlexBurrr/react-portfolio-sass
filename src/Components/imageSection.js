a771e91ac5d88d1c03c9809ebabaf46397c79e4d
Alex Berumen <alexandroberumen@gmail.com>
2021-07-20 21:04:29 -0700

    rm button

--- src/Components/imageSection.js
+++ src/Components/imageSection.js
@@ -1,17 +1,11 @@
 import React from 'react'
 import about from '../images/gradPic.jpg'
-import FileSaver from 'file-saver';
 
 
 
 const ImageSection = () => {
 
-    const ResumeDownload = () => {
-        FileSaver.saveAs(
-            process.env.REACT_APP_CLIENT_URL + "../../public/assets/Alexandro_Berumen_Resume.pdf",
-            "Alexandro_Berumen_Resume.pdf"
-        );
-    }
+
 
     return (
         <div className='ImageSection'>
@@ -23,7 +17,7 @@ const ImageSection = () => {
                 <p>I graduated from UC Berkeley with a degree in Molecular Biology, but have always had my eye on a seperate field. As I spent more and more of my free time learning about the different aspects of software, I realized I wanted a career change.
                     The constant shifts and everchaning landscape in software really intrigued me and once I started building, I knew this was the path I needed to go down. Through the struggles and tiny achievments along the way, I've really enjoyed my journey and look forward to the progress I will make.</p>
 
-                <button className='btn' onClick={ResumeDownload}><a href="../../public/assets/Alexandro_Berumen_Resume.pdf">Download CV</a></button>
+                {/* <button className='btn' ><a href="../../public/assets/Alexandro_Berumen_Resume.pdf">Download CV</a></button> */}
 
 
             </div>
